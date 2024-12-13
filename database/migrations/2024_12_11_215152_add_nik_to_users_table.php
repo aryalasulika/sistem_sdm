9eb7db5bfd2203a805bef0309659bf17d9226195
unknown <aryalasulika28@gmail.com>
2024-12-14 02:19:07 +0700

    perubahan di migration

--- database/migrations/2024_12_11_215152_add_nik_to_users_table.php
+++ database/migrations/2024_12_11_215152_add_nik_to_users_table.php
@@ -11,7 +11,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('nik')->nullable();
+            $table->string('nik', 16)->unique();
         });
     }
 
