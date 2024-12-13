e72dcff2ffa5ba5fd887244af9a580f07285fb1b
unknown <aryalasulika28@gmail.com>
2024-12-13 23:36:00 +0700

    favicon di login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Login | Admin</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="shortcut icon" href="{{ asset('lte/plugins/favicon.ico') }}">
     <style>
         /* Animasi untuk placeholder */
         .form-control:focus::placeholder {
